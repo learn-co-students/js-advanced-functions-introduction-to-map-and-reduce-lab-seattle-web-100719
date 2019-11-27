ab36bf3c3d98d1a99038d71c39425867b56d958f
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-26 16:10:51 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+function mapToNoChange(arr)
+{
+    return arr.map(item => {
+        return item;
+    });
+}
+
+function mapToNegativize(arr)
+{
+    return arr.map(item => {
+        return item * -1;
+    });
+}
+
+function mapToDouble(arr)
+{
+    return arr.map(item => {
+        return item * 2;
+    });
+}
+
+function mapToSquare(arr)
+{
+    return arr.map(item => {
+        return item ** 2;
+    });
+}
+
+function reduceToTotal(arr, startingPoint = 0)
+{
+    return arr.reduce(function(total, element){ return element + total}, startingPoint);
+}
+
+function reduceToAllTrue(arr, startingPoint = true)
+{
+    return arr.reduce(function(total, element){ return element && total}, startingPoint);
+}
+
+function reduceToAnyTrue(arr, startingPoint = false)
+{
+    return arr.reduce(function(total, element){ return element || total}, startingPoint);
+}
